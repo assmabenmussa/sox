8a1f2668dc25dd99b74578ea7dd88cc5daa048e2
unknown <assma.bm@gmail.com>
2021-01-11 21:40:24 +0200

    CODE REFACTOR: removed console logs for production

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,45 +7,35 @@ import { Feedback } from '../feedback'
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  errorMsg: any;
+  errorMsg: any = undefined;
+  successMsg: any = undefined
   submitted: boolean;
   loading: boolean;
   deps: any = ['قسم الـIT', 'قسم الـcore', 'قسم الـMaintenance', 'قسم الـAccess Network', 'الموارد البشرية', 
                'المالية', 'خدمات المشتركين', 'الادارة', 'التسويق', 'المبيعات', 'علم البيانات', 'أفضل عدم الإفصاح'];
   feedback: Feedback;
   constructor(private restService: RestService) { }
-
-
-  //this is a comment 
   
+  ngOnInit() {
+    console.log("new version of the app deployed");
+  }
+
   onSubmit(data){
+    console.log("onSubmit ", data);
+
+
     this.loading = true
-    console.log("Form value", data.value)
     this.restService.postFeedback(data.value)
       .subscribe(res => {
         this.loading = false;
-        console.log("Submitted form response: ", res)
-        JSON.stringify(res)
-      },
-      error => {
+        this.successMsg = res["message"];
+        data.reset();
+      }, error => {
+        console.warn("onSubmit ", error);
         this.loading = false;
-        this.errorMsg = true;
         this.errorMsg = error["errorMessage"];
-        console.log("Leads component error", error["errorMessage"]);
       })
-      data.reset()
   }
   
-  // getAll(){
-  //   this.restService.getF()
-  //   .subscribe(res =>{
-  //     console.log("response: ", res)
-  //     this.feedbackOut = res[0]["feedback"];
-  //   })
-  // }
-
-  ngOnInit() {
-    // this.getAll();
-  }
   
 }
