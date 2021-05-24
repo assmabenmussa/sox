c78523fb6defc174cf9057255cfed6e704f69b67
unknown <assma.bm@gmail.com>
2021-05-24 15:17:35 +0300

    SERVICE: connected url to heroku

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -11,8 +11,8 @@ export class RestService {
     
   }
 
-  // url = 'https://dry-shelf-17630.herokuapp.com/api'; //heroku backend 
-  url = 'http://127.0.0.1:5000/api';
+  url = 'https://dry-shelf-17630.herokuapp.com/api'; //heroku backend 
+  // url = 'http://127.0.0.1:5000/api';
 
   public postFeedback(feedback: Feedback){
     let data = new FormData;
